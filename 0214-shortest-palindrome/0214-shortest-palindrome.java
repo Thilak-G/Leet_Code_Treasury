82773134114af4c277442a359ecb2607ea2b670a
Thilak G <112085725+Thilak-G@users.noreply.github.com>
2024-09-20 10:09:24 +0530

    Time: 8 ms (63.77%), Space: 44.6 MB (66.31%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,23 +1,34 @@
 class Solution {
-    public boolean checkPalin(String s){
-        return (rev(s).equals(s));
+    public String shortestPalindrome(String s) {
+        String temp = rev(s);
+        String combined = s + "#" + temp;
+
+        int[] prefixTable = buildPrefixTable(combined);
+
+        int i = prefixTable[prefixTable.length - 1];
+        String suffix = s.substring(i);
+
+        return rev(suffix) + s;
     }
-    
-    public String rev(String s){
+
+    private int[] buildPrefixTable(String s) {
+        int[] table = new int[s.length()];
+        int j = 0;
+        
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = table[j - 1];
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+            table[i] = j;
+        }
+        return table;
+    }
+
+    public String rev(String s) {
         StringBuilder temp = new StringBuilder(s);
         return temp.reverse().toString();
-    }
-    
-    public String shortestPalindrome(String s) {
-        if(checkPalin(s)){
-            return s;
-        }
-        for(int i = s.length() - 1 ; i >= 0 ; i--){
-            String temp = rev(s.substring(i , s.length())) + s;
-            if(checkPalin(temp)){
-                return temp;
-            }
-        }
-        return s;
     }
 }
