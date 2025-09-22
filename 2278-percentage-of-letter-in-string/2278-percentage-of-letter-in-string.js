e6edb1586a8d15a82902dc5a9d044ce9f82582a4
Thilak G <112085725+Thilak-G@users.noreply.github.com>
2025-09-22 21:46:18 +0530

    Time: 2 ms (5.75%), Space: 54.1 MB (47.13%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+    let count = 0 ;
+    for(let c of s){
+        if(c === letter){
+            count++;
+        }
+    }
+    console.log(count);
+    return Math.floor((count/s.length) * 100) ;
+};
