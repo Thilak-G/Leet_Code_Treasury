4f48ae33791a602d04185e1cf66d4eff951f3f9a
Thilak G <112085725+Thilak-G@users.noreply.github.com>
2026-02-03 20:14:22 +0530

    Time: 2 ms (3.95%), Space: 46.8 MB (17.09%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,60 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findPeak(MountainArray arr , int start , int end){
+        while(start < end){
+            int mid = start + (end - start) / 2 ;
+
+            if(arr.get(mid) > arr.get(mid + 1)){
+                end = mid ;
+            }else{
+                start = mid + 1 ;
+            }
+        }
+        return start ;
+    }
+    public int binarySearch(MountainArray arr , int target , int start , int end , boolean isAsc){
+        while(start <= end){
+            int mid = (start + end) / 2 ;
+            // if(mid >= arr.length()){
+            //     return -1 ;
+            // }
+            int midValue = arr.get(mid) ;
+            if(midValue == target){
+                return mid ;
+            }
+
+            if(isAsc){
+                if(midValue < target){
+                    start = mid + 1 ;
+                }else{
+                    end = mid - 1 ;
+                }
+            }else{
+                if(midValue > target){
+                    start = mid + 1 ;
+                }else{
+                    end = mid - 1 ;
+                }
+            }
+        }
+        return -1 ;
+    }
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int len = mountainArr.length() - 1 ;
+        int peak = findPeak(mountainArr , 0 , len) ;
+        System.out.println(peak);
+        int firstTry = binarySearch(mountainArr , target , 0 , peak , true);
+        if(firstTry != -1){
+            return firstTry ;
+        }
+        return binarySearch(mountainArr , target , peak + 1 , mountainArr.length() - 1 , false);
+    }
+}
